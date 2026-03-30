fd824f786d5bc683626647779628372d403d7fe8
Annika Singh <ac147616@avcol.school.nz>
2026-03-30 23:39:53 +1300

    Created Membership class

---
+++ JyoshinmonKarate/Models/Membership.cs
@@ -0,0 +1,10 @@
+﻿namespace JyoshinmonKarate.Models
+{
+    public class Membership
+    {
+        public int MembershipId { get; set; }
+        public string MembershipName { get; set; }
+        public decimal Cost { get; set; }
+        public string AgeGroup { get; set; }
+    }
+}
