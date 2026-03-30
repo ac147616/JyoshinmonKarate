db005b69ed9748dcf886b4f3caf96f4e18f6888c
Annika Singh <ac147616@avcol.school.nz>
2026-03-30 23:37:34 +1300

    Created Grading class

---
+++ JyoshinmonKarate/Models/Grading.cs
@@ -0,0 +1,13 @@
+﻿namespace JyoshinmonKarate.Models
+{
+    public class Grading
+    {
+        public int GradingId { get; set; }
+        public int ClubId { get; set; }
+        public DateTime GradingDate { get; set; }
+        public DateTime GradingStartTime { get; set; }
+        public DateTime GradingEndTime { get; set; }
+
+        public Club Club { get; set; }
+    }
+}
