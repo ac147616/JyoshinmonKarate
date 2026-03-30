6d4121b3713e99ac240ecf9a59fd5ed01cba3d2b
Annika Singh <ac147616@avcol.school.nz>
2026-03-30 23:49:02 +1300

    Created Payment class

---
+++ JyoshinmonKarate/Models/Payment.cs
@@ -0,0 +1,25 @@
+﻿namespace JyoshinmonKarate.Models
+{
+    public enum PaymentMethods
+    {
+        CreditCard, BankTransfer, Cash, MobilePayment
+    }
+
+    public enum Status
+    {
+        Pending, Completed, Failed
+    }
+
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int MemberId { get; set; }
+        public string PaymentName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateDue { get; set; }
+        public PaymentMethods PaymentMethod { get; set; }
+        public Status Status { get; set; }
+
+        public Member Member { get; set; }
+    }
+}
