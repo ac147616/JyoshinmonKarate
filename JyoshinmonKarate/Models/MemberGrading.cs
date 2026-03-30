ff492e5f241eef891bf4b978f2b4e6340c2df49f
Annika Singh <ac147616@avcol.school.nz>
2026-03-30 23:43:23 +1300

    Created MemberGrading class

---
+++ JyoshinmonKarate/Models/MemberGrading.cs
@@ -0,0 +1,18 @@
+﻿namespace JyoshinmonKarate.Models
+{
+    public class MemberGrading
+    {
+        public int MembershipGradingId { get; set; }
+        public int GradingId { get; set; }
+        public int MembershipId { get; set; }
+        public int BeltBeforeId { get; set; }
+        public int BeltAfterId { get; set; }
+        public bool Passed { get; set; }
+
+        public Grading Grading { get; set; }
+        public Membership Membership { get; set; }
+        public Belt BeltBefore { get; set; }
+        public Belt BeltAfter { get; set; }
+
+    }
+}
