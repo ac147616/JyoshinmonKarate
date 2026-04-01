4c31efa8126989c50a7a5c5db8ef98b341d6c705
Annika Singh <ac147616@avcol.school.nz>
2026-04-02 11:59:07 +1300

    Added scaffolded item for Schedule.cs to create the SchedulesController in the Controllers folder.

---
+++ JyoshinmonKarate/Controllers/SchedulesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JyoshinmonKarate.Areas.Identity.Data;
+using JyoshinmonKarate.Models;
+
+namespace JyoshinmonKarate.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private readonly JyoshinmonKarateContext _context;
+
+        public SchedulesController(JyoshinmonKarateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Schedules
+        public async Task<IActionResult> Index()
+        {
+            var jyoshinmonKarateContext = _context.Schedules.Include(s => s.Club).Include(s => s.Instructor);
+            return View(await jyoshinmonKarateContext.ToListAsync());
+        }
+
+        // GET: Schedules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Club)
+                .Include(s => s.Instructor)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public IActionResult Create()
+        {
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address");
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "InstructorId", "UserId");
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ScheduleId,ClubId,InstructorId,Level,DayOfWeek,StartTime,EndTime")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", schedule.ClubId);
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "InstructorId", "UserId", schedule.InstructorId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", schedule.ClubId);
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "InstructorId", "UserId", schedule.InstructorId);
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ScheduleId,ClubId,InstructorId,Level,DayOfWeek,StartTime,EndTime")] Schedule schedule)
+        {
+            if (id != schedule.ScheduleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(schedule.ScheduleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", schedule.ClubId);
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "InstructorId", "UserId", schedule.InstructorId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Club)
+                .Include(s => s.Instructor)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule != null)
+            {
+                _context.Schedules.Remove(schedule);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return _context.Schedules.Any(e => e.ScheduleId == id);
+        }
+    }
+}
