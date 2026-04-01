adafde9bba2eb5e273d9c43d285e556ee12176b8
Annika Singh <ac147616@avcol.school.nz>
2026-04-02 10:23:48 +1300

    Added scaffolded item for Attendance.cs to create the AttendancesController in the Controllers folder.

---
+++ JyoshinmonKarate/Controllers/AttendancesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JyoshinmonKarate.Areas.Identity.Data;
+using JyoshinmonKarate.Models;
+
+namespace JyoshinmonKarate.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private readonly JyoshinmonKarateContext _context;
+
+        public AttendancesController(JyoshinmonKarateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Attendances
+        public async Task<IActionResult> Index()
+        {
+            var jyoshinmonKarateContext = _context.Attendances.Include(a => a.Member).Include(a => a.Schedule);
+            return View(await jyoshinmonKarateContext.ToListAsync());
+        }
+
+        // GET: Attendances/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Member)
+                .Include(a => a.Schedule)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // GET: Attendances/Create
+        public IActionResult Create()
+        {
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "FirstName");
+            ViewData["ScheduleId"] = new SelectList(_context.Schedules, "ScheduleId", "ScheduleId");
+            return View();
+        }
+
+        // POST: Attendances/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AttendanceId,ScheduleId,MemberId,Date")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "FirstName", attendance.MemberId);
+            ViewData["ScheduleId"] = new SelectList(_context.Schedules, "ScheduleId", "ScheduleId", attendance.ScheduleId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "FirstName", attendance.MemberId);
+            ViewData["ScheduleId"] = new SelectList(_context.Schedules, "ScheduleId", "ScheduleId", attendance.ScheduleId);
+            return View(attendance);
+        }
+
+        // POST: Attendances/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AttendanceId,ScheduleId,MemberId,Date")] Attendance attendance)
+        {
+            if (id != attendance.AttendanceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(attendance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AttendanceExists(attendance.AttendanceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "FirstName", attendance.MemberId);
+            ViewData["ScheduleId"] = new SelectList(_context.Schedules, "ScheduleId", "ScheduleId", attendance.ScheduleId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Member)
+                .Include(a => a.Schedule)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // POST: Attendances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.Attendances.Remove(attendance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AttendanceExists(int id)
+        {
+            return _context.Attendances.Any(e => e.AttendanceId == id);
+        }
+    }
+}
