191d2f684f089bd612086339e1628f42379cc7f6
Annika Singh <ac147616@avcol.school.nz>
2026-04-02 10:52:05 +1300

    Added scaffolded item for Instructor.cs to create the InstructorsController in the Controllers folder.
    Added scaffolded item for Member.cs to create the MembersController in the Controllers folder.

---
+++ JyoshinmonKarate/Controllers/InstructorsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JyoshinmonKarate.Areas.Identity.Data;
+using JyoshinmonKarate.Models;
+
+namespace JyoshinmonKarate.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly JyoshinmonKarateContext _context;
+
+        public InstructorsController(JyoshinmonKarateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Instructors
+        public async Task<IActionResult> Index()
+        {
+            var jyoshinmonKarateContext = _context.Instructors.Include(i => i.Belt).Include(i => i.Club).Include(i => i.User);
+            return View(await jyoshinmonKarateContext.ToListAsync());
+        }
+
+        // GET: Instructors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .Include(i => i.Belt)
+                .Include(i => i.Club)
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.InstructorId == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // GET: Instructors/Create
+        public IActionResult Create()
+        {
+            ViewData["BeltId"] = new SelectList(_context.Belts, "BeltId", "BeltName");
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Instructors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InstructorId,UserId,ClubId,BeltId,DateJoined")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BeltId"] = new SelectList(_context.Belts, "BeltId", "BeltName", instructor.BeltId);
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", instructor.ClubId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", instructor.UserId);
+            return View(instructor);
+        }
+
+        // GET: Instructors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            ViewData["BeltId"] = new SelectList(_context.Belts, "BeltId", "BeltName", instructor.BeltId);
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", instructor.ClubId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", instructor.UserId);
+            return View(instructor);
+        }
+
+        // POST: Instructors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InstructorId,UserId,ClubId,BeltId,DateJoined")] Instructor instructor)
+        {
+            if (id != instructor.InstructorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instructor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstructorExists(instructor.InstructorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BeltId"] = new SelectList(_context.Belts, "BeltId", "BeltName", instructor.BeltId);
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "Address", instructor.ClubId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", instructor.UserId);
+            return View(instructor);
+        }
+
+        // GET: Instructors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .Include(i => i.Belt)
+                .Include(i => i.Club)
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.InstructorId == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // POST: Instructors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor != null)
+            {
+                _context.Instructors.Remove(instructor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstructorExists(int id)
+        {
+            return _context.Instructors.Any(e => e.InstructorId == id);
+        }
+    }
+}
