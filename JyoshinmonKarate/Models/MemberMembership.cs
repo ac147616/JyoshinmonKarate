2eefb33e3936eb662d3e8e82df482a5aca7a9534
Annika Singh <ac147616@avcol.school.nz>
2026-04-22 13:11:03 +1200

    Created MemberMembership class

---
+++ JyoshinmonKarate/Models/MemberMembership.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JyoshinmonKarate.Models
+{
+    public enum MembershipStatus
+    {
+        Active,
+        Inactive,
+        Suspended,
+        Cancelled
+    }
+    public class MemberMembership
+    {
+        public int MemberMembershipId { get; set; }
+
+        [Display(Name = "Member")]
+        public int MemberId { get; set; }
+
+        [Display(Name = "Membership")]
+        public int MembershipId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Membership Status")]
+        public MembershipStatus MembershipStatus { get; set; }
+
+        //A member can have many memberships over time
+        public Member Member { get; set; }
+        //A membership can be used by many members over time
+        public Membership Membership { get; set; }
+    }
+}
