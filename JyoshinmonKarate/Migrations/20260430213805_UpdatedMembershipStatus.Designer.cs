1809b936821f302f7469a76382093be3bbb3fa26
Annika Singh <ac147616@avcol.school.nz>
2026-05-01 09:50:17 +1200

    Added migration after changing MembershipStatus in MemberMembership model

--- JyoshinmonKarate/Migrations/20260430213805_UpdatedMembershipStatus.Designer.cs
+++ JyoshinmonKarate/Migrations/20260430213805_UpdatedMembershipStatus.Designer.cs
@@ -4,6 +4,7 @@ using JyoshinmonKarate.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JyoshinmonKarate.Migrations
 {
     [DbContext(typeof(JyoshinmonKarateContext))]
-    partial class JyoshinmonKarateContextModelSnapshot : ModelSnapshot
+    [Migration("20260430213805_UpdatedMembershipStatus")]
+    partial class UpdatedMembershipStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
