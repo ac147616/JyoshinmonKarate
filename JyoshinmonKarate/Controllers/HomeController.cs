50a0085d552a59af9e29fbd770ef7835c89e49f4
Annika Singh <ac147616@avcol.school.nz>
2026-06-28 15:27:16 +1200

    Updated Home Controller

--- JyoshinmonKarate/Controllers/HomeController.cs
+++ JyoshinmonKarate/Controllers/HomeController.cs
@@ -1,25 +1,52 @@
-using JyoshinmonKarate.Models;
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using JyoshinmonKarate.Areas.Identity.Data;
+using JyoshinmonKarate.Data;
+using JyoshinmonKarate.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JyoshinmonKarate.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly JyoshinmonKarateContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(JyoshinmonKarateContext context, UserManager<User> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Member> members = new List<Member>();
+
+            if (User.Identity != null && User.Identity.IsAuthenticated && User.IsInRole("User"))
+            {
+                string userId = _userManager.GetUserId(User);
+
+                List<Member> allMembers = await _context.Members
+                    .Include("Belt")
+                    .Include("Club")
+                    .ToListAsync();
+
+                foreach (Member member in allMembers)
+                {
+                    if (member.UserId == userId)
+                    {
+                        members.Add(member);
+                    }
+                }
+            }
+
+            return View(members);
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
